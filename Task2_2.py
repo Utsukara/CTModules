57b5b3bc2474d5bf503bbc1e51cfc6fceabdfdc1
Utsukara <160966817+Utsukara@users.noreply.github.com>
2024-04-08 17:37:41 -0500

    Create Task2_2.py

---
+++ Task2_2.py
@@ -0,0 +1,9 @@
+# Task 2: Date Extractor
+
+# Problem Statement: Write a Python program that uses the datetime module to extract and display the current month and year. Additionally, allow the user to input a date string and parse it into a datetime object.
+# Code Example:
+# # main.py
+# from datetime import datetime
+# # Implement code to display the current month and year
+# # Implement code to parse a user-input date string into a datetime object
+# Expected Outcome: The program should accurately display the current month and year and successfully convert a user-input date string (e.g., "2023-03-15") into a datetime object, handling any invalid inputs gracefully.
